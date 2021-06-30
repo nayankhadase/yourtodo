c299cdc71a2f71398fc1dda23c6658c25cff1ddd
Nayan <khadsenayan1996@gmail.com>
2021-06-30 15:37:31 +0530

    first commit

--- todoproj/settings.py
+++ todoproj/settings.py
@@ -136,7 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-# STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
